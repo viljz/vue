a34df9f03e2740b777c7e519657a10572c3ff89e
viljz <201469098@qq.com>
2019-05-29 18:25:48 +0800

    回退测试

---
+++ src/components/cubcomponts/subComments.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="subcmt_container">
+    <h3 class="title">发表评论</h3>
+    <textarea maxlength="120" placeholder="最多输入120个字"></textarea>
+    <mt-button type="primary" size="large">发表评论</mt-button>
+
+    <div class="cmt_list" v-for="(item, i) in contents" :key="item._id">
+      <div class="cmt_title">第{{ i + 1 }}楼&nbsp;&nbsp;用户:{{ item.name }}&nbsp;&nbsp;发表时间: {{ item.add_time }}</div>
+      <div class="cmt_body">{{ item.content }}</div>
+    </div>
+
+    <mt-button type="danger" size="large" plain>加载更多</mt-button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      contents: {}
+    };
+  },
+  created() {
+    this.getComment()
+  },
+  methods: {
+    getComment() {
+      this.$http.get('pinglun').then(result => {
+        this.contents = result.body
+        console.log(result.body)
+      })
+    }
+  },
+  // 根据 id 获取当前文章的 所有评论
+  props: ["id"]
+};
+</script>
+<style lang="scss" scoped>
+.subcmt_container {
+  .title {
+    font-size: 16px;
+  }
+  textarea {
+    font-size: 12px;
+  }
+  .cmt_list {
+    margin: 5px;
+    border: 1px solid #eee;
+    border-radius: 5px;
+    padding: 5px;
+    font-size: 12px;
+    .cmt_body {
+      text-indent: 2em;
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,22 +6,29 @@ Vue.use(VueRouter)
 // 导入 vue-router
 import vueResource from 'vue-resource'
 Vue.use(vueResource)
+Vue.http.options.root = 'http://localhost:3000'
+// 导入格式化时间插件
+// import moment from 'moment'
+// Vue.filter('dateFormat', function (pattern = "YYYY-MM-DD HH:mm:ss") {
+//   return moment(Date.now()).format(pattern)
+// })
 
 
 // 导入 { Mint-UI } 样式
 import './lib/mui/css/mui.min.css'
 import './lib/mui/css/icons-extra.css'
-import { Header, Swipe, SwipeItem } from 'mint-ui'
+import { Header, Swipe, SwipeItem, Button } from 'mint-ui'
 Vue.component(Header.name, Header)
-Vue.component(Swipe.name, Swipe);
-Vue.component(SwipeItem.name, SwipeItem);
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
+Vue.component(Button.name, Button)
+
 
 // 导入自己的路由
 import router from './router'
 
 // 导入 app 根组件
 import app from './App.vue'
-import VueResource from 'vue-resource';
 
 // 创建实例
 const vm = new Vue({
